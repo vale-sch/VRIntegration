92d7c9b049d27bc32f0e2bcb5784a022f89399bf
vale-sch <vali.vogt@live.de>
2022-10-07 20:48:54 +0200

    another try

--- mainTest.js
+++ mainTest.js
@@ -1,258 +1,147 @@
-
-
-
-
-
-
-//--------------------------------------WEBXR-----------------------------------------------------
-
-import { WebXRButton } from "./webxr-button.js";
-import { Renderer, createWebGLContext } from './renderer.js';
-import { Scene } from './scene.js';
-import { SkyboxNode } from './skybox.js';
-
-let xrButton = null;
-let xrRefSpace = null;
-
-let gl = null;
-let scene = new Scene();
-
-let renderer = null;
-
-scene.addNode(new SkyboxNode({ url: 'fudgelogo.png' }));
-function initXR() {
-
-    // Adds a helper button to the page that indicates if any XRDevices are
-    // available and let's the user pick between them if there's multiple.
-    xrButton = new WebXRButton({
-        onRequestSession: onRequestSession,
-        onEndSession: onEndSession
-    });
-    document.querySelector('header').appendChild(xrButton.domElement);
-    // Is WebXR available on this UA?
-
-    if (navigator.xr) {
-        // If the device allows creation of exclusive sessions set it as the
-        // target of the 'Enter XR' button.
-        navigator.xr.isSessionSupported('immersive-vr').then((supported) => {
-            xrButton.enabled = supported;
-        });
-    }
-
-}
-
-
-
-
-
-function onRequestSession() {
-    return navigator.xr.requestSession('immersive-vr').then(onSessionStarted);
-}
-
-
-
-
-function onSessionStarted(session) {
-    // This informs the 'Enter XR' button that the session has started and
-    // that it should display 'Exit XR' instead.
-    xrButton.setSession(session);
-
-    // Listen for the sessions 'end' event so we can respond if the user
-    // or UA ends the session for any reason.
-    session.addEventListener('end', onSessionEnded);
-
-    // Create a WebGL context to render with, initialized to be compatible
-    // with the XRDisplay we're presenting to.
-    gl = createWebGLContext({
-        xrCompatible: true
-    });
-
-    // Create a renderer with that GL context (this is just for the samples
-    // framework and has nothing to do with WebXR specifically.)
-    renderer = new Renderer(gl);
-    // Set the scene's renderer, which creates the necessary GPU resources.
-    scene.setRenderer(renderer);
-
-    // Use the new WebGL context to create a XRWebGLLayer and set it as the
-    // sessions baseLayer. This allows any content rendered to the layer to
-    // be displayed on the XRDevice.
-    session.updateRenderState({ baseLayer: new XRWebGLLayer(session, gl) });
-
-    // Get a frame of reference, which is required for querying poses. In
-    // this case an 'local' frame of reference means that all poses will
-    // be relative to the location where the XRDevice was first detected.
-    session.requestReferenceSpace('local').then((refSpace) => {
-        xrRefSpace = refSpace;
-
-        // Inform the session that we're ready to begin drawing.
-        session.requestAnimationFrame(onXRFrame);
-    });
-    console.log(scene);
-}
-
-
-
-
-function onEndSession(session) {
-    session.end();
-}
-
-function onSessionEnded(event) {
-    xrButton.setSession(null);
-
-    // In this simple case discard the WebGL context too, since we're not
-    // rendering anything else to the screen with it.
-    renderer = null;
-}
-
-
-
-
-
-function onXRFrame(t, frame) {
-    let session = frame.session;
-
-    // Per-frame scene setup. Nothing WebXR specific here.
-    scene.startFrame();
-
-    // Inform the session that we're ready for the next frame.
-    session.requestAnimationFrame(onXRFrame);
-
-    // Get the XRDevice pose relative to the Frame of Reference we created
-    // earlier.
-    let pose = frame.getViewerPose(xrRefSpace);
-
-    // Getting the pose may fail if, for example, tracking is lost. So we
-    // have to check to make sure that we got a valid pose before attempting
-    // to render with it. If not in this case we'll just leave the
-    // framebuffer cleared, so tracking loss means the scene will simply
-    // disappear.
-    if (pose) {
-        let glLayer = session.renderState.baseLayer;
-
-        // If we do have a valid pose, bind the WebGL layer's framebuffer,
-        // which is where any content to be displayed on the XRDevice must be
-        // rendered.
-        gl.bindFramebuffer(gl.FRAMEBUFFER, glLayer.framebuffer);
-
-        // Clear the framebuffer
-        gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-
-        // Loop through each of the views reported by the frame and draw them
-        // into the corresponding viewport.
-        for (let view of pose.views) {
-            let viewport = glLayer.getViewport(view);
-            gl.viewport(viewport.x, viewport.y,
-                viewport.width, viewport.height);
-
-            // Draw this view of the scene. What happens in this function really
-            // isn't all that important. What is important is that it renders
-            // into the XRWebGLLayer's framebuffer, using the viewport into that
-            // framebuffer reported by the current view, and using the
-            // projection matrix and view transform from the current view.
-            // We bound the framebuffer and viewport up above, and are passing
-            // in the appropriate matrices here to be used when rendering.
-            scene.draw(view.projectionMatrix, view.transform);
-        }
-    } else {
-        // There's several options for handling cases where no pose is given.
-        // The simplest, which these samples opt for, is to simply not draw
-        // anything. That way the device will continue to show the last frame
-        // drawn, possibly even with reprojection. Alternately you could
-        // re-draw the scene again with the last known good pose (which is now
-        // likely to be wrong), clear to black, or draw a head-locked message
-        // for the user indicating that they should try to get back to an area
-        // with better tracking. In all cases it's possible that the device
-        // may override what is drawn here to show the user it's own error
-        // message, so it should not be anything critical to the application's
-        // use.
-    }
-
-    // Per-frame scene teardown. Nothing WebXR specific here.
-    scene.endFrame();
-}
-initXR();
-//--------------------------------------WEBXR-----------------------------------------------------
-
-
-
-
-
-
-
-
-//--------------------------------------FUDGE-----------------------------------------------------
 var f = FudgeCore;
-var ƒAid = FudgeAid;
-let graphId = document.head.querySelector("meta[autoView]").getAttribute("autoView")
-
-window.addEventListener("load", startInteractiveViewport(graphId));
-let cmpCamera = null;
-let canvas = null;
+window.addEventListener("load", init);
 let viewport = null;
-// show dialog for startup, user interaction required e.g. for starting audio
-let graph = null;
+let cmpCamera = null;
+async function init() {
+    await FudgeCore.Project.loadResources("Internal.json");
+    FudgeCore.Debug.log("Project:", FudgeCore.Project.resources);
+    let madeMazeGraph = f.Project.resources[document.head.querySelector("meta[autoView]").getAttribute("autoView")];
 
-// setup and start interactive viewport
-async function startInteractiveViewport(_graphId) {
-    // load resources referenced in the link-tag
-    await f.Project.loadResourcesFromHTML();
-    f.Debug.log("Project:", f.Project.resources);
-
-    // get the graph to show from loaded resources
-    graph = f.Project.resources[_graphId];
-    f.Debug.log("Graph:", graph);
-    if (!graph) {
+    FudgeCore.Debug.log("Graph:", madeMazeGraph);
+    if (!madeMazeGraph) {
         alert("Nothing to render. Create a graph with at least a mesh, material and probably some light");
         return;
     }
 
     // setup the viewport
     cmpCamera = new f.ComponentCamera();
-    canvas = document.querySelector("canvas");
+    let canvas = document.querySelector("canvas");
     viewport = new f.Viewport();
-    viewport.initialize("InteractiveViewport", graph, cmpCamera, canvas);
-    f.Debug.log("Viewport:", viewport);
-    // make the camera interactive (complex method in FudgeAid)
-    let cameraOrbit = ƒAid.Viewport.expandCameraToInteractiveOrbit(viewport);
+    viewport.initialize("InteractiveViewport", madeMazeGraph, cmpCamera, canvas);
 
-    // hide the cursor when interacting, also suppressing right-click menu
-    canvas.addEventListener("mousedown", canvas.requestPointerLock);
-    canvas.addEventListener("mouseup", function () { document.exitPointerLock(); });
 
-    // setup audio
-    let cmpListener = new f.ComponentAudioListener();
-    cmpCamera.node.addComponent(cmpListener);
-    f.AudioManager.default.listenWith(cmpListener);
-    f.AudioManager.default.listenTo(graph);
-    f.Debug.log("Audio:", f.AudioManager.default);
 
-    // draw viewport once for immediate feedback
-    f.Render.prepare(cameraOrbit);
-    viewport.draw();
-    let node = new f.Node();
-    console.log(node);
-    // dispatch event to signal startup done
-    canvas.dispatchEvent(new CustomEvent("interactiveViewportStarted", { bubbles: true, detail: viewport }));
+
+
+
+    // viewport.draw();
+    // f.Loop.addEventListener(f.EVENT.LOOP_FRAME, update);
+    //  f.Loop.start();
 }
-/*--------------------------------------FUDGE-----------------------------------------------------
+
+function update(_event) {
+    viewport.draw();
+}
 
 
-*/
+navigator.xr.addEventListener('devicechange', checkForXRSupport);
+checkForXRSupport();
+async function checkForXRSupport() {
+    // Check to see if there is an XR device available that supports immersive VR
+    // presentation (for example: displaying in a headset). If the device has that
+    // capability the page will want to add an "Enter VR" button to the page (similar to
+    // a "Fullscreen" button) that starts the display of immersive VR content.
+    navigator.xr.isSessionSupported('immersive-vr').then((supported) => {
+        if (supported) {
+            var enterXrBtn = document.createElement("button");
+            enterXrBtn.innerHTML = "Enter VR";
+            enterXrBtn.addEventListener("click", beginXRSession);
+            document.body.appendChild(enterXrBtn);
+        } else {
+            console.log("Session not supported: " + reason);
+        }
+    });
+}
+function beginXRSession() {
+    // requestSession must be called within a user gesture event
+    // like click or touch when requesting an immersive session.
+    navigator.xr.requestSession('immersive-vr')
+        .then(onSessionStarted)
+        .catch(err => {
+            // May fail for a variety of reasons. Probably just want to
+            // render the scene normally without any tracking at this point.
+            console.log(err);
+        });
+}
+let xrSession = null;
+let xrReferenceSpace = null;
 
+function onSessionStarted(session) {
+    // Store the session for use later.
+    xrSession = session;
 
+    xrSession.requestReferenceSpace('local')
+        .then((referenceSpace) => {
+            xrReferenceSpace = referenceSpace;
+        })
+        .then(setupWebGLLayer) // Create a compatible XRWebGLLayer
+        .then(() => {
+            // Start the render loop
+            xrSession.requestAnimationFrame(onDrawFrame);
+        });
+}
+let glCanvas = document.querySelector("canvas");
+let gl = glCanvas.getContext("webgl");
+//loadSceneGraphics(gl);
 
+function setupWebGLLayer() {
+    // Make sure the canvas context we want to use is compatible with the current xr device.
+    return gl.makeXRCompatible().then(() => {
+        // The content that will be shown on the device is defined by the session's
+        // baseLayer.
+        xrSession.updateRenderState({ baseLayer: new XRWebGLLayer(xrSession, gl) });
+    });
+}
+function onDrawFrame(timestamp, xrFrame) {
+    // Do we have an active session?
+    if (xrSession) {
+        let glLayer = xrSession.renderState.baseLayer;
+        let pose = xrFrame.getViewerPose(xrReferenceSpace);
+        if (pose) {
+            // Run imaginary 3D engine's simulation to step forward physics, animations, etc.
+            // viewport.draw();
 
+            gl.bindFramebuffer(gl.FRAMEBUFFER, glLayer.framebuffer);
 
+            for (let view of pose.views) {
+                let viewport = glLayer.getViewport(view);
+                gl.viewport(viewport.x, viewport.y, viewport.width, viewport.height);
+                drawScene(view);
+            }
+        }
+        // Request the next animation callback
+        xrSession.requestAnimationFrame(onDrawFrame);
+    } else {
+        // No session available, so render a default mono view.
+        gl.viewport(0, 0, glCanvas.width, glCanvas.height);
+        drawSceneFromDefaultView();
 
-
-
-
-
-
-
-
-
-
-
-
+        // Request the next window callback
+        window.requestAnimationFrame(onDrawFrame);
+    }
+}
+function drawScene(view) {
+    /* cmpCamera.mtxLocal.position = new f.Vector3(
+         view.transform.position.x,
+         view.transform.position.y,
+         view.transform.position.z,
+     );
+ 
+     cmpCamera.setOrientationQuaternion(
+         view.transform.orientation.x,
+         view.transform.orientation.y,
+         view.transform.orientation.z,
+         view.transform.orientation.w,
+     );
+ 
+     cmpCamera.setProjectionMatrix4x4(
+         view.projectionMatrix[0],
+         view.projectionMatrix[1],
+         //...
+         view.projectionMatrix[14],
+         view.projectionMatrix[15]
+     );
+ 
+     scene.renderWithCamera(camera);*/
+    //  console.log("JO");
+}
